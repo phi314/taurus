75b3203c3febb1108871ee620cff2c9435e5c1a6
Arief Wibowo <rriev.wibowo@gmail.com>
2016-10-11 22:10:52 +0700

    adding rfid jquery

---
+++ application/logs/log-2016-10-11.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2016-10-11 21:58:46 --> Config Class Initialized
+INFO - 2016-10-11 21:58:46 --> Hooks Class Initialized
+DEBUG - 2016-10-11 21:58:46 --> UTF-8 Support Enabled
+INFO - 2016-10-11 21:58:46 --> Utf8 Class Initialized
+INFO - 2016-10-11 21:58:46 --> URI Class Initialized
+DEBUG - 2016-10-11 21:58:46 --> No URI present. Default controller set.
+INFO - 2016-10-11 21:58:46 --> Router Class Initialized
+INFO - 2016-10-11 21:58:46 --> Output Class Initialized
+INFO - 2016-10-11 21:58:46 --> Security Class Initialized
+DEBUG - 2016-10-11 21:58:46 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-10-11 21:58:46 --> Input Class Initialized
+INFO - 2016-10-11 21:58:46 --> Language Class Initialized
+INFO - 2016-10-11 21:58:46 --> Loader Class Initialized
+INFO - 2016-10-11 21:58:46 --> Helper loaded: unleashed_helper
+INFO - 2016-10-11 21:58:46 --> Helper loaded: url_helper
+INFO - 2016-10-11 21:58:46 --> Database Driver Class Initialized
+DEBUG - 2016-10-11 21:58:46 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2016-10-11 21:58:47 --> Session: Class initialized using 'files' driver.
+INFO - 2016-10-11 21:58:47 --> Controller Class Initialized
+INFO - 2016-10-11 21:58:47 --> Config Class Initialized
+INFO - 2016-10-11 21:58:47 --> Hooks Class Initialized
+DEBUG - 2016-10-11 21:58:47 --> UTF-8 Support Enabled
+INFO - 2016-10-11 21:58:47 --> Utf8 Class Initialized
+INFO - 2016-10-11 21:58:47 --> URI Class Initialized
+INFO - 2016-10-11 21:58:47 --> Router Class Initialized
+INFO - 2016-10-11 21:58:47 --> Output Class Initialized
+INFO - 2016-10-11 21:58:47 --> Security Class Initialized
+DEBUG - 2016-10-11 21:58:47 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-10-11 21:58:47 --> Input Class Initialized
+INFO - 2016-10-11 21:58:47 --> Language Class Initialized
+INFO - 2016-10-11 21:58:47 --> Loader Class Initialized
+INFO - 2016-10-11 21:58:47 --> Helper loaded: unleashed_helper
+INFO - 2016-10-11 21:58:47 --> Helper loaded: url_helper
+INFO - 2016-10-11 21:58:47 --> Database Driver Class Initialized
+DEBUG - 2016-10-11 21:58:47 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2016-10-11 21:58:47 --> Session: Class initialized using 'files' driver.
+INFO - 2016-10-11 21:58:47 --> Controller Class Initialized
+INFO - 2016-10-11 21:58:47 --> Model Class Initialized
+INFO - 2016-10-11 21:58:47 --> Helper loaded: inflector_helper
+INFO - 2016-10-11 21:58:47 --> Model Class Initialized
+INFO - 2016-10-11 21:58:47 --> Model Class Initialized
+INFO - 2016-10-11 21:58:47 --> Helper loaded: absensi_helper
+INFO - 2016-10-11 21:58:47 --> Model Class Initialized
+INFO - 2016-10-11 21:58:47 --> Model Class Initialized
+INFO - 2016-10-11 21:58:48 --> Model Class Initialized
+INFO - 2016-10-11 21:58:48 --> Model Class Initialized
+INFO - 2016-10-11 21:58:48 --> File loaded: /media/phi314/Data/Web Development/taurus/application/views/absensi_machine/list_absensi_view.php
+INFO - 2016-10-11 21:58:48 --> File loaded: /media/phi314/Data/Web Development/taurus/application/views/templates/_parts/public_master_sidebar_view.php
+INFO - 2016-10-11 21:58:48 --> File loaded: /media/phi314/Data/Web Development/taurus/application/views/templates/_parts/absensi_machine_master_header_view.php
+INFO - 2016-10-11 21:58:48 --> File loaded: /media/phi314/Data/Web Development/taurus/application/views/templates/_parts/public_master_footer_view.php
+INFO - 2016-10-11 21:58:48 --> File loaded: /media/phi314/Data/Web Development/taurus/application/views/templates/absensi_machine_master_view.php
+INFO - 2016-10-11 21:58:48 --> Final output sent to browser
+DEBUG - 2016-10-11 21:58:48 --> Total execution time: 1.3347
